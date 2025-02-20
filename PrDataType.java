0c6cbafa75d38bc61b30ed3d9359c98427dc0368
Affan_Ali <mohdaffanali2006@gmail.com>
2025-02-20 10:50:50 +0530

    Add files via upload
    primitive datatypes in java

---
+++ PrDataType.java
@@ -0,0 +1,14 @@
+class PrDataType{
+   public static void main(String[] args){ 
+   int myNum=5;
+   float myFloatNum=5.99f;
+   char myLetter='d';
+   boolean myBool=true;
+   String myText="hello";
+   System.out.println(myNum);
+   System.out.println(myFloatNum);
+   System.out.println(myLetter);
+   System.out.println(myBool);
+   System.out.println(myText);
+  }
+  }
