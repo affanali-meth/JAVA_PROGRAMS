0bcf1484f2cc44b044013aac16ea330a09201b44
Affan_Ali <mohdaffanali2006@gmail.com>
2025-03-06 10:45:09 +0530

    java program for console method

---
+++ ConsoleExam.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+class ConsoleExam{
+   public static void main(String[] args){
+   System.out.println("using console");
+   Console con=System.console();
+   if(con!=null){
+           con.printf("enter your name");
+           String name=con.readLine();
+           con.printf("hello,%s!%n",name);
+  }else{
+          System.out.println("console not available");
+    }
+}
+}
+  
