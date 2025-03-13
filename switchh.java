dee026966033bda165df8a8c3b7676e83bbc1b74
Affan_Ali <mohdaffanali2006@gmail.com>
2025-03-13 09:55:35 +0530

    java program for switch case

---
+++ switchh.java
@@ -0,0 +1,42 @@
+class switchh {
+   public static void main(String[ ] args){
+         int day_no=4;
+         String day;
+         
+         switch (day_no){
+            case 1:
+                day="sunday";
+                break;
+            case 2:
+                day="monday";
+                break;
+             case 3:
+                day="tuesday";
+                break;
+              case 4:
+                 day="wednesday";
+                 break;
+               case 5:
+                 day="thursday";
+                 break;
+               case 6:
+                 day="friday";
+                 break;
+                case 7:
+                  day="saturday";
+                  break;
+               default:
+                  day="invalid day";
+                  break;
+        }
+        System.out.println(day);
+}
+        }   
+
+
+
+
+
+
+
+
